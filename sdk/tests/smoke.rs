0b46703d6e9f58aef5d69bbc769bce77d24873e1
Santiago <santiago@carmuega.me>
2025-12-29 16:43:39 -0300

    test: introduce example serde smoke tests

---
+++ sdk/tests/smoke.rs
@@ -0,0 +1,27 @@
+// for each .tii file in the examples directory, try to decode the tii file using the sdk
+use std::fs;
+
+use tx3_sdk::tii::Protocol;
+
+#[test]
+fn test_tii_files() {
+    let manifest_dir = env!("CARGO_MANIFEST_DIR");
+
+    let examples_dir = format!("{manifest_dir}/../examples");
+
+    let tii_files = fs::read_dir(examples_dir).unwrap();
+
+    for file in tii_files {
+        let path = file.unwrap().path();
+
+        if path.extension().unwrap() != "tii" {
+            continue;
+        }
+
+        let tii = fs::read_to_string(path.clone())
+            .map_err(|e| eprintln!("Error reading file {}: {}", path.display(), e))
+            .unwrap();
+
+        let _ = Protocol::from_string(tii).unwrap();
+    }
+}
